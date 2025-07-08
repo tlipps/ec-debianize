0f96f18d3de3e39b97c03a28d8b72ee11457dbe6
Thomas Lipps <thomas.lipps@ubica-robotics.eu>
2025-07-08 16:55:02 +0200

    setup get_source, simple changelog, do not set ethercat conf

--- get_source.sh
+++ get_source.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DEB_VERSION=`(cd etherlabmaster && dpkg-parsechangelog | sed -rne 's/^Version: ([0-9.]+(\+[0-9]*hg[0-9a-z]*)?).*$$/\1/p')`
+DEB_VERSION="1.33"
 DEB_ORIG="etherlabmaster_$DEB_VERSION.orig.tar.gz"
 
-HG_REPOS="http://hg.code.sf.net/p/etherlabmaster/code"
-HG_COMMIT=`(echo "$DEB_VERSION" | sed -rne 's/^[0-9.]+\+[0-9]*hg([0-9a-z]*)p([0-9a-z]*).*$$/\1/p')`
-HG_DIR="etherlabmaster-hg"
+HG_REPOS="https://github.com/ribalda/ethercat.git"
+HG_COMMIT="97e7ee64a6b4a2e3847270797cae681b8484a7d0"
+HG_DIR="ethercat"
 
-PATCH_REPOS="http://hg.code.sf.net/u/uecasm/etherlab-patches"
-PATCH_COMMIT=`(echo "$DEB_VERSION" | sed -rne 's/^[0-9.]+\+[0-9]*hg([0-9a-z]*)p([0-9a-z]*).*$$/\2/p')`
+# PATCH_REPOS="http://hg.code.sf.net/u/uecasm/etherlab-patches"
+# PATCH_COMMIT=`(echo "$DEB_VERSION" | sed -rne 's/^[0-9.]+\+[0-9]*hg([0-9a-z]*)p([0-9a-z]*).*$$/\2/p')`
 
 function cleanup {
   rm -rf "$HG_DIR"
@@ -17,16 +17,19 @@ trap cleanup EXIT
 set -e
 
 echo "Cloning HG commit $HG_COMMIT..."
-hg clone $HG_REPOS "$HG_DIR" -r $HG_COMMIT
+git clone $HG_REPOS
+cd "$HG_DIR"
+git checkout $HG_COMMIT
+cd ..
 
-echo "Cloning patchset commit $PATCH_COMMIT..."
-hg clone $PATCH_REPOS "$HG_DIR/.hg/patches" -r $PATCH_COMMIT
+# echo "Cloning patchset commit $PATCH_COMMIT..."
+# hg clone $PATCH_REPOS "$HG_DIR/.hg/patches" -r $PATCH_COMMIT
 
-echo "Apply patchset..."
-(cd $HG_DIR && hg qpush -a)
+# echo "Apply patchset..."
+# (cd $HG_DIR && hg qpush -a)
 
 echo "Create source archive..."
-(cd $HG_DIR && hg archive "../$DEB_ORIG")
+tar czf $DEB_ORIG $HG_DIR
 
 echo "Extracting source..."
 (cd etherlabmaster && tar xfz "../$DEB_ORIG" --strip 1)
